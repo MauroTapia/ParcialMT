29f8930b64d968c3ad33e9f5ec5b0701bb683ad9
tu_nombre_de_usuario <maurotapiadev@gmail.com>
2023-08-16 20:44:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './components/Formulario';
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Formulario</h1>
+      <Formulario />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  const [nombre, setNombre] = useState('');
+  const [auto, setAuto] = useState('');
+  const [errorNombre, setErrorNombre] = useState('');
+  const [errorAuto, setErrorAuto] = useState('');
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (nombre.length < 2) {
+      setErrorNombre('El nombre debe tener al menos 2 caracteres');
+      return;
+    } else {
+      setErrorNombre('');
+    }
+
+    if (auto.length < 2) {
+      setErrorAuto('El nombre del auto debe tener al menos 2 caracteres');
+      return;
+    } else {
+      setErrorAuto('');
+    }
+
+    setSubmittedData({ nombre, auto });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre:</label>
+          <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          {errorNombre && <p className="error">{errorNombre}</p>}
+        </div>
+        <div>
+          <label>Auto:</label>
+          <input type="text" value={auto} onChange={(e) => setAuto(e.target.value)} />
+          {errorAuto && <p className="error">{errorAuto}</p>}
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      {submittedData && (<p>
+          Hola {submittedData.nombre}, tu auto elegido es {submittedData.auto}.
+        </p>)}
+    </div>
+  );
+}
+
+export default App;
